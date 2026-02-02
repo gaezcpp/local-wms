14ef466c861fd76a3bbd496b1d001e7b1aadcd01
gaezcpp <muhammad.gaez@cpp.co.id>
2026-02-02 08:57:07 +0700

    try fix ZeroDivisionError

--- wms_inherit_stock_barcode/models/stock_quant.py
+++ wms_inherit_stock_barcode/models/stock_quant.py
@@ -11,21 +11,35 @@ class InheritStockQuant(models.Model):
     exp_group = fields.Datetime(string="Exp Group", tracking=True)
     
     def _recompute_package_pallet_status(self):
-        pembagi_pallet = self.env['ir.config_parameter'].sudo().get_param('pembagi_pallet')
-        if not pembagi_pallet:
+        param = self.env['ir.config_parameter'].sudo().get_param('pembagi_pallet')
+        if not param:
             raise ValidationError("pembagi_pallet belum disetting!")
-        
+
+        try:
+            pembagi_pallet = float(param)
+        except:
+            raise ValidationError("pembagi_pallet bukan angka!")
+
+        if pembagi_pallet == 0:
+            raise ValidationError("pembagi_pallet tidak boleh 0!")
+
         packages = self.mapped('package_id').filtered(lambda p: p)
         for pkg in packages:
             pallet_status = 'eceran'
-            quants = pkg.contained_quant_ids
+            quants = pkg.contained_quant_ids.filtered(lambda q: q.quantity > 0)
 
             product_ids = quants.mapped('product_id')
             if len(product_ids) == 1 and quants:
                 quant = quants[0]
                 uom_pallet = quant.product_id.uom_pallet_id
-                if (uom_pallet.factor / float(pembagi_pallet)) / quant.quantity == 1:
-                    pallet_status = 'full_pallet'
+
+                if uom_pallet and quant.quantity:
+                    try:
+                        result = (uom_pallet.factor / pembagi_pallet) / quant.quantity
+                        if abs(result - 1) < 0.00001:
+                            pallet_status = 'full_pallet'
+                    except ZeroDivisionError:
+                        pass
 
             if pkg.pallet_status != pallet_status:
                 pkg.pallet_status = pallet_status
